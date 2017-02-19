96d565ccf7d11f82a05eef851083a2e6807a9533
Arek Krolak <x155110@wp.pl>
2017-02-19 22:52:07 +0100

    Add existing file

--- SimpleSplitter.sol
+++ SimpleSplitter.sol
@@ -2,14 +2,9 @@ pragma solidity ^0.4.2;
 
 contract SimpleSplitter {
     address owner;
-
     address adAlice;  //initiated as eth.accounts[1] - this typically isn't coinbase, so if you send ether from website it will not be split
     address adBob;    //in order to split do something like this in geth: 
     address adCarol;  //eth.sendTransaction({from: eth.accounts[1], to: "0x3c45260ef6a547d06949feea65ea38e6eee32f54", value: web3.toWei(1, "ether")})  
-
-    int balAlice = 0;
-    int balBob = 0;
-    int balCarol = 0;
 
     function SimpleSplitter(address ada, address adb, address adc) payable {
         owner = msg.sender;
@@ -22,28 +17,21 @@ contract SimpleSplitter {
       return this.balance;
     }
 
-    function getNoBalance(int no) constant returns (int) {
-
-      if (no == 1) return balAlice;
-      if (no == 2) return balBob;
-      if (no == 3) return balCarol;
-
-      return 0;
+    function getAddress(int no) constant returns (address) {
+      if (no == 1) return adAlice;
+      if (no == 2) return adBob;
+      if (no == 3) return adCarol;
+      return address(0);
     }
 
     /* splits received amount, sends to Bob and Carol, adjusts balances */
-    function  split(uint amt) private returns(bool sufficient)  {
-      if (this.balance < amt) return false;
-      uint half1 = amt/2;
-      uint half2 = amt - half1;  // half1 = half2 to the precision of rounding
-      balBob += int(half1);
-      balCarol += int(half2);
+    function  split(uint amt) private  {
+     uint half1 = amt/2;
+     uint half2 = amt - half1;  // half1 = half2 to the precision of rounding
       if(!adBob.send(half1))
           throw;
-      // no event since they are expensive
       if(!adCarol.send(half2))
           throw;
-      return true;
     }
 
     function killMe() returns (bool) {
@@ -54,11 +42,7 @@ contract SimpleSplitter {
     }
 
     function () payable {
-      if (msg.sender == adAlice) {
-        if (!split(msg.value)) {
-          balAlice -= int(msg.value);
-        }
-      }
+      if (msg.sender == adAlice) { split(msg.value); }
     }
 
 }
